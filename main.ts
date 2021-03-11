5e1f56e27d05139693a5f8ba4edbbf45159a7d93
dfghjkjhgr <67027632+dfghjkjhgr@users.noreply.github.com>
2021-03-10 21:06:18 -0500

    Restore '1/10/2021, 1:48:58 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     man.move(-1)
 })
+input.onGesture(Gesture.FreeFall, function () {
+    score += 100
+})
 input.onGesture(Gesture.LogoUp, function () {
     man.change(LedSpriteProperty.Y, 1)
 })
@@ -16,12 +19,6 @@ function GameOver () {
     croc2.delete()
     croc3.delete()
     man.delete()
-    d.delete()
-    h.delete()
-    o.delete()
-    r.delete()
-    r.delete()
-    r2.delete()
     music.playTone(932, music.beat(BeatFraction.Whole))
     music.playTone(831, music.beat(BeatFraction.Whole))
     music.playTone(784, music.beat(BeatFraction.Whole))
@@ -54,27 +51,19 @@ function GameOver () {
     basic.showNumber(score)
 }
 let Animation: Image = null
-let d: game.LedSprite = null
-let r2: game.LedSprite = null
-let r: game.LedSprite = null
-let o: game.LedSprite = null
-let h: game.LedSprite = null
 let score = 0
 let croc3: game.LedSprite = null
 let croc2: game.LedSprite = null
 let croc1: game.LedSprite = null
 let man: game.LedSprite = null
+led.setBrightness(128)
 man = game.createSprite(0, 2)
 croc1 = game.createSprite(1, 2)
 croc2 = game.createSprite(2, 2)
 croc3 = game.createSprite(3, 2)
 score = 0
-h = game.createSprite(4, 0)
-o = game.createSprite(4, 1)
-r = game.createSprite(4, 2)
-r2 = game.createSprite(4, 3)
-d = game.createSprite(4, 4)
 let speed = 800
+let level = 1
 let directionCroc1 = 1
 let DirectionCroc2 = -1
 let DirectionCroc3 = 1
@@ -89,30 +78,13 @@ basic.forever(function () {
     if (man.isTouching(croc3)) {
         GameOver()
     }
-    if (man.isTouching(h)) {
-        score += 1
+    if (man.get(LedSpriteProperty.X) == 4) {
         man.set(LedSpriteProperty.X, 0)
-        speed += -50
-    }
-    if (man.isTouching(o)) {
+        man.set(LedSpriteProperty.Y, 2)
         score += 1
-        man.set(LedSpriteProperty.X, 0)
+        level += 1
         speed += -50
-    }
-    if (man.isTouching(r)) {
-        score += 1
-        man.set(LedSpriteProperty.X, 0)
-        speed += -50
-    }
-    if (man.isTouching(r2)) {
-        score += 1
-        man.set(LedSpriteProperty.X, 0)
-        speed += -50
-    }
-    if (man.isTouching(d)) {
-        score += 1
-        man.set(LedSpriteProperty.X, 0)
-        speed += -50
+        music.playTone(587, music.beat(BeatFraction.Whole))
     }
 })
 basic.forever(function () {
@@ -143,5 +115,27 @@ basic.forever(function () {
         DirectionCroc3 = 1
     } else {
         croc3.change(LedSpriteProperty.Y, DirectionCroc3)
+    }
+})
+basic.forever(function () {
+    if (level >= 16) {
+        man.delete()
+        croc1.delete()
+        croc2.delete()
+        croc3.delete()
+        croc3.delete()
+        basic.showString("You beat the game!!!!!!!!")
+        for (let index = 0; index < 4; index++) {
+            music.playTone(131, music.beat(BeatFraction.Whole))
+            music.playTone(233, music.beat(BeatFraction.Whole))
+            music.playTone(185, music.beat(BeatFraction.Whole))
+            music.playTone(185, music.beat(BeatFraction.Whole))
+            music.playTone(494, music.beat(BeatFraction.Half))
+            music.playTone(220, music.beat(BeatFraction.Half))
+            music.playTone(370, music.beat(BeatFraction.Half))
+            music.playTone(185, music.beat(BeatFraction.Half))
+            basic.showString("Final Score:")
+            basic.showNumber(score)
+        }
     }
 })
